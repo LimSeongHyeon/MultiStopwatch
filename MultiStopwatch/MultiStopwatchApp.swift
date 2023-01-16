c4f7fa8f4b98003a058516622b1923b5204925b1
LimSeongHyeon <shot7351@gmail.com>
2023-01-16 19:05:57 +0900

    Initial Commit

---
+++ MultiStopwatch/MultiStopwatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiStopwatchApp.swift
+//  MultiStopwatch
+//
+//  Created by Lim on 2023/01/16.
+//
+
+import SwiftUI
+
+@main
+struct MultiStopwatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
